369b8ee59f61bc5882dcd14861f0fb2253dd00b9
Zumardi Rahman <zumar2014@gmail.com>
2022-10-08 16:27:35 +0700

    CRUD API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import UserRoute from "./routes/UserRoute.js";
 
 const app=express();
 
@@ -8,7 +8,7 @@ const app=express();
 app.use(cors());
 //express json
 app.use(express.json());
-
+app.use(UserRoute);
 //server run
 app.listen(5000, ()=> console.log(`server berjalan....`));
 
