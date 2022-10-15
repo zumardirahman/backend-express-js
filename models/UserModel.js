7c1c2862fed8aa886e38098af85534ff71314fd7
Zumardi Rahman <zumar2014@gmail.com>
2022-10-15 10:42:32 +0700

    register user

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,20 +1,27 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
 
-const {DataTypes}   = Sequelize;
+const { DataTypes } = Sequelize;
 
-const User = db.define('users',{
-    name : DataTypes.STRING,
+const User = db.define(
+  "users",
+  {
+    name: { type: DataTypes.STRING },
     email: DataTypes.STRING,
-    gender: DataTypes.STRING
-},{
-    freezeTableName:true
-})
+    gender: DataTypes.STRING,
+    password: DataTypes.STRING,
+    refresh_token: DataTypes.TEXT,
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 export default User;
 
 //fungsi genegerate tabel tidak ada di database
 
-(async()=>{
-    await db.sync()
-})();
+// aktifkan ini jika tidak ada table
+// (async () => {
+//   await db.sync();
+// })();
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getUsers, getUserById, createUser,updateUser,deleteUser} from "../controllers/UserController.js"
+import {getUsers, getUserById, createUser,updateUser,deleteUser, register} from "../controllers/UserController.js"
 
 const router = express.Router()
 
@@ -8,5 +8,7 @@ router.get('/users/:id',getUserById)
 router.post('/users',createUser)
 router.patch('/users/:id',updateUser)
 router.delete('/users/:id',deleteUser)
+
+router.post('/users/register',register)
 
 export default router;
