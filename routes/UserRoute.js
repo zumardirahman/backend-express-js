d9625710ccc2049bc42393cd3f5c6c2cf54574d5
Zumardi Rahman <zumar2014@gmail.com>
2022-10-15 17:48:19 +0700

    verify token route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -5,10 +5,10 @@ import {verifyToken} from "../middleware/VerifyToken.js" //utk verifikasi endpoi
 const router = express.Router()
 
 router.get('/users',verifyToken, getUsers)
-router.get('/users/:id',getUserById)
-router.post('/users',createUser)
-router.patch('/users/:id',updateUser)
-router.delete('/users/:id',deleteUser)
+router.get('/users/:id',verifyToken,getUserById)
+router.post('/users',verifyToken,createUser)
+router.patch('/users/:id',verifyToken,updateUser)
+router.delete('/users/:id',verifyToken,deleteUser)
 
 
 export default router;
